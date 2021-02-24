d28e93a0b7ee8ef6c9e256e75885381db0599450
tanguy <tanguy.jouvin@hotmail.com>
2021-02-25 00:47:17 +0100

    update database2

--- Vue/Convocation/index.php
+++ Vue/Convocation/index.php
@@ -1,6 +1,6 @@
 <?php $this->titre = "Convocation"; ?>
 <table>
-    <?php foreach ($convocations as $convocation): ?>
+    <?php foreach($convocations as $convocation): ?>
         <tr>
             <td> <?= $this->nettoyer($convocation['categorie']) ?> </td>
             <td> <?= $this->nettoyer($convocation['competition']) ?> </td>
