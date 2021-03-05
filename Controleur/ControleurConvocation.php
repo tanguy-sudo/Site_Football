e22461366113affa68da20c63a681292481f0209
nairod49 <78696310+nairod49@users.noreply.github.com>
2021-03-05 16:34:04 +0100

    Update ControleurConvocation.php

--- Controleur/ControleurConvocation.php
+++ Controleur/ControleurConvocation.php
@@ -20,5 +20,19 @@ class ControleurConvocation extends Controleur {
                                 'dateChoisi'         => $date
                            ));
     }
+    
+     public function ajoutConvocation() {
+        if($this->EntraineurisConnected()){
+        	
+         $date = empty($this->requete->getParametre("date")) ? date("Y-m-d") : implode('-', array_reverse(explode('/', $this->requete->getParametre("date"))));
+        $convocations = $this->convocation->getConvocation($date);
+        $effectifs = $this->convocation->geteffectifConv();
+        $calendrier =$this->convocation->getrencontre($date);
+           $this->genererVue(array('convocations' => $convocations,
+                                'effectifs'  => $effectifs,
+                                'dateChoisi' => $date,
+                                'rencontre' => $calendrier));  
+        }
+    } 
 
 }
