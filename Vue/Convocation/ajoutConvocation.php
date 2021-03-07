fa07c216d436af544aaec894820f955a398d9433
nairod49 <78696310+nairod49@users.noreply.github.com>
2021-03-07 20:33:02 +0100

    Update ajoutConvocation.php

--- Vue/Convocation/ajoutConvocation.php
+++ Vue/Convocation/ajoutConvocation.php
@@ -17,3 +17,43 @@
         <h5 class="text-center">Aucune rencontre pour cette date</h5>
     
     <?php endif; ?>
+    <form class="row g-3" method="post" action="convocation/valideConvocation">
+<?php foreach($rencontre as $rencontres): ?>
+            <div class="col s4">
+                <div class="card-panel grey lighten-5">
+                
+                    <table class="table">
+                        <tr><th scope="col">Compétition : </td><td> <?= $this->nettoyer($rencontres['competition']) ?> </td></tr>
+                        <tr><th scope="col">Equipe adverse :</td><td> <?= $this->nettoyer($rencontres['equipeAdverse']) ?> </td></tr>
+                        <tr><th scope="col">Site :</td><td> <?= $this->nettoyer($rencontres['site']) ?> </td></tr>
+                        <tr><th scope="col">Terrain : </td><td> <?= $this->nettoyer($rencontres['terrain']) ?> </td></tr>
+                        <tr><th scope="col">Heure : </td><td> <?= $this->nettoyer($rencontres['heure']) ?> </td></tr>
+                        
+                        <tr><th scope="col">Equipe : </td><td> <?= $this->nettoyer($rencontres['equipe']) ?> </td></tr>
+                    
+                        <tr><th class="text-center" scope="col" colspan="2">Liste des joueurs :</td></tr>
+                        
+   
+        <?php for ($i=0;$i<14;$i++) :?>
+        <div class="form-floating mb-3">
+                   <?php echo $i+1; ?>
+                    <select class="form-select" name="idEffectif<?php echo $i; ?>"  id="floatingSelect" >
+								<option value="null"> </option>                       
+                        <?php foreach($effectifs as $effectif): ?>
+                            <option value="<?= $this->nettoyer($effectif['id_effectif']) ?>"> <?= $this->nettoyer($effectif['prenom']).' '. $this->nettoyer($effectif['nom']); ?> </option>         
+                        <?php endforeach; ?>
+                    </select>
+                    <label for="floatingSelect">Effectifs</label>
+        </div>
+        <?php endfor; ?>
+         <input type="hidden" name="ekip" value="<?= $this->nettoyer($rencontres['equipe']) ?>" /> 
+           <button type="submit" name="ajout" class="btn btn-primary mb-3">Valider</button>
+  </form>
+        
+   
+   <?php endforeach; ?> 
+
+     </table>
+                </div>
+            </div>
+            </div>
